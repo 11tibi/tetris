252553055252ff10a7d1864ac96926f28ff1e3b6
11tibi <69271302+11tibi@users.noreply.github.com>
2022-09-11 18:15:39 +0300

    Update collision detection

--- src/js/Block.ts
+++ src/js/Block.ts
@@ -1,5 +1,4 @@
 import {BOARD} from "./constants.js";
-import {fields} from "./Fields.js"
 
 enum BlockType {
     STRAIGHT,
@@ -19,7 +18,6 @@ export class Block {
     private readonly colorType: string;
     private rotation: number;
 
-
     constructor(ctx: CanvasRenderingContext2D, x: number, y: number) {
         this.ctx = ctx;
         this.X = x;
@@ -27,6 +25,34 @@ export class Block {
         this.type = BlockType[BlockType[Math.floor(Math.random() * Object.keys(BlockType).length / 2)]];
         this.colorType = ColorType[Math.floor(Math.random() * ColorType.length)];
         this.rotation = 0;
+    }
+
+    public getX(): number {
+        return this.X;
+    }
+
+    public getY(): number {
+        return this.Y;
+    }
+
+    public setX(x: number): void {
+        this.X = x;
+    }
+
+    public setY(y: number): void {
+        this.Y = y;
+    }
+
+    public setRotation(rotation: number): void {
+        this.rotation = rotation;
+    }
+
+    public getType(): BlockType {
+        return this.type;
+    }
+
+    public getRotation(): number {
+        return this.rotation;
     }
 
     public draw(): void {
@@ -46,7 +72,7 @@ export class Block {
         }
     }
 
-    private clear(): void {
+    public clear(): void {
         /**
          * Clear the current block
          */
@@ -67,41 +93,38 @@ export class Block {
         /**
          * Drop the piece
          */
-        while (!this.checkCollision()) {
-            this.Y++;
-        }
-        this.Y--;
+        // while (!this.checkCollision()) {
+        //     this.Y++;
+        // }
+        // this.Y--;
     }
 
     private moveDown(): void {
-        this.clear();
         this.Y++;
-        if (this.checkCollision()) {
-            this.Y--;
-            // generate next piece
-        }
-        this.draw();
     }
 
     private moveSide(side: string): void {
+        /**
+         * Move the piece to left or right
+         * @param {side} direction where the piece will be moved it should be "right" or "left"
+         */
         if (side === "left") {
             this.X--;
-            if (this.checkCollision()) {
-                this.X++;
-            }
         } else if (side === "right") {
             this.X++;
-            if (this.checkCollision()) {
-                this.X--;
-            }
         } else {
             throw new Error('"side" must be left or right');
         }
 
     }
 
-    public move(key: string): void {
-        this.clear();
+    public move(key: string): Array<number> {
+        /**
+         * Move the piece to a certain direction
+         * @param {key} the string representation of the key pressed
+         * @returns The last position and rotation of the piece
+         */
+        const lastPosition: Array<number> = [this.X, this.Y, this.rotation];
         switch (key) {
             case "ArrowRight": this.moveSide("right"); break;
             case "ArrowLeft": this.moveSide("left"); break;
@@ -109,37 +132,11 @@ export class Block {
             case "ArrowUp": this.drop(); break;
             case " ": this.rotate(); break;
         }
-        this.draw();
-    }
-
-    public checkCollision(): boolean {
-        const currFigure: any = BOARD.FIGURES[this.type][this.rotation % (BOARD.FIGURES[this.type].length)];
-        for (let i=0; i<4; i++) {
-            for (let j=0; j<4; j++) {
-                if (currFigure.includes(i * 4 + j)) {
-                    let x_ind: number = this.X + j;
-                    let y_ind: number = this.Y + i;
-                    if (x_ind < 0 || x_ind >= BOARD.SQUARES_X || y_ind < 0 || y_ind >= BOARD.SQUARES_Y) {
-                        console.log(`x = ${x_ind} y = ${y_ind}`);
-                        return true;
-                    }
-                    if (fields[y_ind][x_ind]) {
-                        console.log(`x = ${x_ind} y = ${y_ind}`);
-                        return true;
-                    }
-                }
-
-            }
-        }
-        return false;
+        return lastPosition;
     }
 
     private rotate(): void {
         this.rotation++;
-        if (this.checkCollision()) {
-            this.rotation--;
-        }
-        // TODO check if rotation is possible
     }
 
     public toString(): string {
--- src/js/Board.ts
+++ src/js/Board.ts
@@ -4,17 +4,18 @@ import {Block} from "./Block.js";
 export class Board {
     private readonly ctx: CanvasRenderingContext2D;
     private fields: Array<Array<boolean>>;
+    private currentPiece: Block;
+    private lastPosition: Array<number>;
 
     constructor(ctx: CanvasRenderingContext2D) {
         this.ctx = ctx;
         this.fields = [];
         for (let i=0; i<BOARD.SQUARES_Y; i++) {
             this.fields.push([]);
-            for (let j=0; j<BOARD.BOARD_START_X; j++) {
+            for (let j=0; j<BOARD.SQUARES_X; j++) {
                 this.fields[i].push(false);
             }
         }
-        console.log(this.fields);
     }
 
     public draw(): void {
@@ -29,17 +30,101 @@ export class Board {
         this.ctx.stroke();
     }
 
+    public checkCollision(): boolean {
+        const currFigure: any = BOARD.FIGURES[this.currentPiece.getType()]
+            [this.currentPiece.getRotation() % (BOARD.FIGURES[this.currentPiece.getType()].length)];
+        for (let i=0; i<4; i++) {
+            for (let j=0; j<4; j++) {
+                if (currFigure.includes(i * 4 + j)) {
+                    let x_ind: number = this.currentPiece.getX() + j;
+                    let y_ind: number = this.currentPiece.getY() + i;
+                    if (x_ind < 0 || x_ind >= BOARD.SQUARES_X || y_ind < 0 || y_ind >= BOARD.SQUARES_Y) {
+                        console.log(`x = ${x_ind} y = ${y_ind}`);
+                        return true;
+                    }
+                    if (this.fields[y_ind][x_ind]) {
+                        console.log(`x = ${x_ind} y = ${y_ind}`);
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private freeze(): void {
+        const currFigure: any = BOARD.FIGURES[this.currentPiece.getType()]
+            [this.currentPiece.getRotation() % (BOARD.FIGURES[this.currentPiece.getType()].length)];
+        for (let i=0; i<4; i++) {
+            for (let j=0; j<4; j++) {
+                if (currFigure.includes(i * 4 + j)) {
+                    let x_ind: number = this.currentPiece.getX() + j;
+                    let y_ind: number = this.currentPiece.getY() + i;
+                    this.fields[y_ind][x_ind] = true;
+                }
+            }
+        }
+    }
+
+    public checkFreeze(): boolean {
+        const currFigure: any = BOARD.FIGURES[this.currentPiece.getType()]
+            [this.currentPiece.getRotation() % (BOARD.FIGURES[this.currentPiece.getType()].length)];
+        for (let i=0; i<4; i++) {
+            for (let j=0; j<4; j++) {
+                if (currFigure.includes(i * 4 + j)) {
+                    let x_ind: number = this.currentPiece.getX() + j;
+                    let y_ind: number = this.currentPiece.getY() + i;
+                    if (y_ind === BOARD.SQUARES_Y - 1) {
+                        return true;
+                    }
+                    if (this.fields[y_ind+1][x_ind]) {
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return false
+    }
+
+    private deleteFullRows(): void {
+        for (let i=0; i<BOARD.SQUARES_Y; i++) {
+            if (this.fields[i].every(Boolean)) {
+                
+            }
+        }
+    }
+
     public run(): void {
         this.draw();
-        let block = new Block(this.ctx, 3, -1);
+        this.currentPiece = new Block(this.ctx, 3, 0);
         window.addEventListener('keydown', (event) => {
-            block.move(event.key);
+            this.currentPiece.clear();
+            this.lastPosition = this.currentPiece.move(event.key);
+            if (this.checkCollision()) {
+                this.currentPiece.setX(this.lastPosition[0]);
+                this.currentPiece.setY(this.lastPosition[1]);
+                this.currentPiece.setRotation(this.lastPosition[2]);
+            }
+            this.currentPiece.draw();
         });
 
         setInterval(() => {
-            block.checkCollision();
-            block.draw();
-            block.move("ArrowDown");
+            if (this.checkFreeze()) {
+                this.freeze();
+                this.currentPiece = new Block(this.ctx, 3, 0);
+            } else {
+                this.currentPiece.clear();
+                this.lastPosition = this.currentPiece.move("ArrowDown");
+                this.currentPiece.draw();
+                if (this.checkCollision()) {
+                    this.currentPiece.clear();
+                    this.currentPiece.setX(this.lastPosition[0]);
+                    this.currentPiece.setY(this.lastPosition[1]);
+                    this.currentPiece.draw();
+                }
+            }
         }, 600)
     }
 }
