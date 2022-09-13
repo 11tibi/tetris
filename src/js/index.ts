75acfb7c863a734f411fcc6b3c7ab1734929b5df
11tibi <69271302+11tibi@users.noreply.github.com>
2022-09-13 19:26:19 +0300

    Add message for mobile devices

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,11 +1,19 @@
 import {Board} from './Board.js';
 
-window.onload = window.onresize = function () {
+window.onload = function () {
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
-    let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    let board = new Board(ctx);
-    board.run();
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        canvas.style.visibility = 'hidden';
+        let div = document.createElement('div')
+        div.innerHTML += 'Mobile version not available';
+        div.className = 'text';
+        document.body.appendChild(div);
+    } else {
+        let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        let board = new Board(ctx);
+        board.run();
+    }
 }
 
